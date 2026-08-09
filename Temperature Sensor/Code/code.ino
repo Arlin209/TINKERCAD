79f6663ceb1034415c458928d65c2e0380df6320
Arlin209 <arlinwilson18@gmail.com>
2026-08-09 16:20:23 +0530

    Add files via upload

---
+++ Temperature Sensor/Code/code.ino
@@ -0,0 +1,12 @@
+void setup(){
+  Serial.begin(9600);
+}
+void loop(){
+  int sensorValue = analogRead(A0);
+  float voltage = sensorValue*5.0/1023.0;
+  float temperature = (voltage-0.5)*100.0;
+   Serial.print("Temperature");
+   Serial.print(temperature);
+   Serial.println("C");
+  delay(500);
+}
